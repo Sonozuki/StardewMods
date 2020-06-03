614e83e6f13c5d4bba94fc8eaf0aede535c486a7
Andy Crocker <atmcrocker@gmail.com>
2020-06-03 11:52:20 +0100

    [MoreTrees] Add initial project files

---
+++ MoreTrees/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace MoreTrees
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /// <summary>The mod entry point.</summary>
+        /// <param name="helper">Provides methods for interacting with the mod directory as well as the modding api.</param>
+        public override void Entry(IModHelper helper)
+        {
+            ApplyHarmonyPatches();
+
+            this.Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+
+
+        /// <summary>Apply the harmony patches.</summary>
+        private void ApplyHarmonyPatches()
+        {
+            // TODO: apply patches
+        }
+
+        /// <summary>Invoked when the player loads a save.</summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event data.</param>
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            // TODO: load content packs
+        }
+    }
+}
---
+++ MoreTrees/Models/TreeData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace MoreTrees.Models
+{
+    /// <summary>Represents a tree content.json file.</summary>
+    public class TreeData
+    {
+        /// <summary>The item the tree drops when using a tap on it.</summary>
+        public TappingProduct TappingProduct { get; set; }
+
+        /// <summary>The item the tree drops when it gets cut down.</summary>
+        public string WoodProduct { get; set; }
+
+        /// <summary>The item to plant to grow it.</summary>
+        public string Seed { get; set; }
+
+        /// <summary>The tree will only get loaded if one of the listed mods is present.</summary>
+        public List<string> IncludeIfModIsPresent { get; set; }
+
+        /// <summary>The tree will only get loaded if none of the listed mods are present.</summary>
+        public List<string> ExcludeIfModIsPresent { get; set; }
+
+        /// <summary>Whether this tree required the user to have the extended mode of MoreTrees.</summary>
+        public bool RequiresExtendedMode { get; set; }
+
+        /// <summary>The item the tree drops when using the 'Barn Remover' tool on it.</summary>
+        public string BarkProduct { get; set; }
+
+        /// <summary>The items the tree can drop whenever it's shaked.</summary>
+        public List<ShakeProduct> ShakingProduct { get; set; }
+    }
+}
