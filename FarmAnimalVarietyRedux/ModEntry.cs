95117071122214e64b4d97b8e1de7b3d71a123a1
Andy Crocker <atmcrocker@gmail.com>
2020-11-11 15:15:39 +0000

    [FarmAnimalVarietyRedux] Add initial project files

---
+++ FarmAnimalVarietyRedux/ModEntry.cs
@@ -0,0 +1,19 @@
+﻿using StardewModdingAPI;
+using System;
+
+namespace FarmAnimalVarietyRedux
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>The mod entry point.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
