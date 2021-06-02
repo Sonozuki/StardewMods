6390f5a530e026741757bb0203415baee28448c7
Andy Crocker <atmcrocker@gmail.com>
2021-06-02 08:10:30 +0100

    [MasterFisher] Add initial project files

---
+++ MasterFisher/ModEntry.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+
+namespace MasterFisher
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>The mod entry point.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+    }
+}
