1f72569c1464e6280b7bb4ba6356315816618c3d
Andy Crocker <atmcrocker@gmail.com>
2021-07-04 18:47:53 +0100

    [SatoCore] Add initial project files

---
+++ SatoCore/ModBase.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+
+namespace SatoCore
+{
+    /// <summary>Represents the base of a mod that depends on SatoCore.</summary>
+    public abstract class ModBase : Mod
+    {
+        /*********
+        ** Public Methods
+        *********/
+        /// <inheritdoc/>
+        public sealed override void Entry(IModHelper helper) { }
+    }
+}
---
+++ SatoCore/ModEntry.cs
@@ -0,0 +1,16 @@
+﻿using StardewModdingAPI;
+
+namespace SatoCore
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public Methods
+        *********/
+        /// <inheritdoc/>
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+}
