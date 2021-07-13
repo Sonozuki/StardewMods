dfeeaef344cde28ba8af00406d1e0b9dcab2e2df
Andy Crocker <atmcrocker@gmail.com>
2021-07-13 19:28:41 +0100

    [SatoCore] Add action to core (instead of each mod individually)

---
+++ SatoCore/Action.cs
@@ -0,0 +1,15 @@
+﻿namespace SatoCore
+{
+    /// <summary>The ways that data in a model can be interpreted.</summary>
+    public enum Action
+    {
+        /// <summary>The data will be added.</summary>
+        Add,
+
+        /// <summary>The data will edit previous data.</summary>
+        Edit,
+
+        /// <summary>The data will delete previous data.</summary>
+        Delete
+    }
+}
