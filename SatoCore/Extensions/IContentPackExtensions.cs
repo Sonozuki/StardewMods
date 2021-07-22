070e5e5cf3fbbae1b513d6c19129c33381fba0a2
Andy Crocker <atmcrocker@gmail.com>
2021-07-22 12:57:46 +0100

    [SatoCore] Add TryLoadAsset<T>(stirng, out T) to IContentPack

---
+++ SatoCore/Extensions/IContentPackExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Content;
+using StardewModdingAPI;
+using System;
+using System.IO;
+
+namespace SatoCore.Extensions
+{
+    /// <summary>Extension methods for the <see cref="IContentPack"/> interface.</summary>
+    public static class IContentPackExtensions
+    {
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>Loads content from the content pack folder.</summary>
+        /// <typeparam name="T">The expected data type to load.</typeparam>
+        /// <param name="contentPack">The content pack to to load content from.</param>
+        /// <param name="key">The relative file path within the content pack (case-insensitive).</param>
+        /// <param name="result">The loaded asset.</param>
+        /// <returns><see langword="true"/>, if the loading was successful; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="key"/> is empty or contains invalid characters.</exception>
+        /// <exception cref="ContentLoadException">Thrown if the asset couldn't be loaded (e.g. because it doesn't exist).</exception>
+        public static bool TryLoadAsset<T>(this IContentPack contentPack, string key, out T result)
+            where T : class
+        {
+            if (File.Exists(Path.Combine(contentPack.DirectoryPath, key)))
+            {
+                result = contentPack.LoadAsset<T>(key);
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
