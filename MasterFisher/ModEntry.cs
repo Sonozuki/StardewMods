f2d3d3e3c9d31ce38dc2a5c7e56e1d16f502becf
Andy Crocker <atmcrocker@gmail.com>
2021-07-22 11:59:21 +0100

    [MasterFisher] Clear loaded bait when reloading a save

--- MasterFisher/ModEntry.cs
+++ MasterFisher/ModEntry.cs
@@ -63,6 +63,7 @@ namespace MasterFisher
         {
             CategoriesBeingLoaded.Clear();
             LocationAreasBeingLoaded.Clear();
+            BaitBeingLoaded.Clear();
         }
 
         /// <inheritdoc/>
