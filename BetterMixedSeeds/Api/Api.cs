f054754134bb9aa7f5a350f004c1fca3f1cc0df2
Andy Crocker <atmcrocker@gmail.com>
2022-08-19 00:54:14 +0100

    [BetterMixedSeeds] Add ability to reinclude excluded crops, update to .net5

--- BetterMixedSeeds/Api/Api.cs
+++ BetterMixedSeeds/Api/Api.cs
@@ -1,4 +1,9 @@
-﻿namespace BetterMixedSeeds
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetterMixedSeeds
 {
     /// <summary>Provides basic crop apis.</summary>
     public class Api : IApi
@@ -7,13 +12,52 @@
         ** Public Methods
         *********/
         /// <inheritdoc/>
+        public string[] GetExcludedCrops() => ModEntry.Instance.CropsToExclude.ToArray();
+
+        /// <inheritdoc/>
         public void ForceExcludeCrop(params string[] cropNames)
         {
+            if (!Context.IsWorldReady)
+                throw new InvalidOperationException("Cannot exclued crops when a save isn't loaded.");
+
             if (cropNames == null || cropNames.Length == 0)
                 return;
 
             ModEntry.Instance.Monitor.Log($"A mod has forcibly excluded: {string.Join(", ", cropNames)}");
             ModEntry.Instance.CropsToExclude.AddRange(cropNames);
+
+            // reload the seeds to refelect newly excluded crops
+            ModEntry.Instance.LoadEnabledSeeds();
+        }
+
+        /// <inheritdoc/>
+        public string[] ReincludeCrop(params string[] cropNames)
+        {
+            if (!Context.IsWorldReady)
+                throw new InvalidOperationException("Cannot reinclude crops when a save isn't loaded.");
+
+            if (cropNames == null || cropNames.Length == 0)
+                return new string[0];
+
+            var failedToRemoveCrops = new List<string>();
+
+            // try to reinclude specified crops
+            foreach (var cropName in cropNames)
+            {
+                var numberOfRemovedEntries = ModEntry.Instance.CropsToExclude.RemoveAll(name => name == cropName);
+                if (numberOfRemovedEntries == 0)
+                    failedToRemoveCrops.Add(cropName);
+            }
+
+            // log the crops that were successfully reincluded
+            var reincludedCrops = cropNames.Except(failedToRemoveCrops);
+            if (reincludedCrops.Any())
+                ModEntry.Instance.Monitor.Log($"A mod has forcibly excluded: {string.Join(", ", reincludedCrops)}");
+
+            // reload the seeds to refelect newly included crops
+            ModEntry.Instance.LoadEnabledSeeds();
+
+            return failedToRemoveCrops.ToArray();
         }
     }
 }
